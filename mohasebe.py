53ba77727b99722a0bb2c74c007553fa3d10a817
amiramman12 <flah64009@gmail.com>
2025-10-27 22:57:25 +0330

    Add files via upload

---
+++ mohasebe.py
@@ -0,0 +1,9 @@
+﻿y=int(input("سال تولد"))
+x=int(input("سال کنونی"))
+age_years=x-y
+age_months=age_year*12
+age_days=age_years*365
+age_hours=age_days*24
+age_minuts=age_hours*60
+age_seconds=age_minuts*60
+print("عمر به سال="age_years,"عمر به ماه="age_months,"عمر به روز"age_days,"عمر به ساعت="age_hours,"عمر به دقيقه"age_minuts,"عمر به ثانيه"age_second
